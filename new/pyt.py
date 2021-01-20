2aaf260ba024ac59b497ac810aa2ef2e8f9d5be3
syed969 <syed.khalandar@newpage.io>
2021-01-20 17:58:43 +0530

    msg

--- new/pyt.py
+++ new/pyt.py
@@ -1,20 +1,6 @@
 print("syed mohammed khalander")
 print()
 
-
-print("syed mohammed khalander")
-
-print("syed mohammed khalander")
-
-print("syed mohammed khalander")
-
-print("syed mohammed khalander")
-print("syed mohammed khalander")
-print("syed mohammed khalander")
-print("syed mohammed khalander")
-print("syed mohammed khalander")
-print("syed mohammed khalander")
-
 print("y")
 print("s")
 print("k")
